5370328d4716236ab2ae0635554d084b66b3ec52
StefanGreve <greve.stefan@outlook.jp>
2021-10-09 18:42:25 +0200

    Add PowerShell profile

---
+++ Documents/WindowsPowerShell/profile.ps1
@@ -0,0 +1,55 @@
+$global:psrc = "$Home\Documents\WindowsPowerShell\profile.ps1"
+
+#region Aliases
+
+function Invoke-Anonfile {
+    python -m anonfile.__init__ $Args
+}
+
+Set-Alias -Name anonfile -Value Invoke-Anonfile
+
+function Update-Configuration {
+    git --git-dir="$HOME\Desktop\repos\confiles" --work-tree=$HOME $Args
+}
+
+Set-Alias -Name config -Value Update-Configuration
+
+#endregion
+
+#region Command Prompt
+
+function Write-BranchName {
+    try {
+        $branch = git rev-parse --abbrev-ref HEAD
+
+        if ($branch -eq "HEAD") {
+            $branch = git rev-parse --short HEAD
+            Write-Host " ($branch)" -ForegroundColor "red" -NoNewline
+        }
+        else {
+            Write-Host " ($branch)" -ForegroundColor "blue" -NoNewline
+        }
+    }
+    catch {
+        Write-Host " (no branches yet)" -ForegroundColor "yellow" -NoNewline
+    }
+}
+
+function prompt {
+    Write-Host '[' -NoNewline
+    Write-Host $env:UserName -NoNewline -ForegroundColor Cyan
+    Write-Host "@$env:ComputerName " -NoNewline
+    $path = "$($executionContext.SessionState.Path.CurrentLocation)"
+    $userPrompt = "`n$('>' * ($nestedPromptLevel + 1)) "
+
+    if (Test-Path .git) {
+        Write-BranchName
+    }
+
+    Write-Host $path -NoNewline -ForegroundColor Green
+    Write-Host ']' -NoNewline
+
+    return $userPrompt
+}
+
+#endregion
