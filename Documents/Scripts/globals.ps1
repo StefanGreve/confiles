31fd6276fcd2a80187cdcba3e52752a66d6f52b6
StefanGreve <greve.stefan@outlook.jp>
2023-06-18 20:08:45 +0200

    Create file for global variable definitions

---
+++ Documents/Scripts/globals.ps1
@@ -0,0 +1,4 @@
+$global:VSRC = "$env:APPDATA\Code\User\settings.json"
+$global:VIRC = "$env:LOCALAPPDATA\nvim\init.vim"
+$global:WTRC = "$env:LOCALAPPDATA\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\settings.json"
+$global:WGRC = "$env:LOCALAPPDATA\Packages\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe\LocalState\settings.json"
